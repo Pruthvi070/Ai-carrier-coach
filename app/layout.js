ff4c95fad20f003bd8c008cd7bc3cde428839ff7
Pruthviraj Chavan <chavanpruthviraj9595@gmail.com>
2025-02-26 16:18:53 +0530

    Initial commit

---
+++ app/layout.js
@@ -0,0 +1,50 @@
+/* eslint-disable react/jsx-no-undef */
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+const inter = Inter({ subsets: ["latin"] });
+
+
+
+
+export const metadata = {
+  title: "AI Career Coach",
+  description: "",
+};
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider     appearance={{
+      baseTheme: dark,
+    }}>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${inter.className}`}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {/* {header} */}
+            <Header />
+
+            <main className="min-h-screen">
+              {children}
+            </main>
+            {/* Footer */}
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>Made with 💗 by Pruthviraj </p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/jsx-no-undef */
+import { Button } from "@/components/ui/button"
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Next.js!</h1>
+      <br />
+
+      <Button>
+      heloo
+      </Button>
+
+    </div>
+  );
+}
