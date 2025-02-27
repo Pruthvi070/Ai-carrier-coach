5fda71d0d3dce8a520b1d5589f570b186ce68510
Pruthviraj Chavan <chavanpruthviraj9595@gmail.com>
2025-02-27 16:31:57 +0530

    Storing user in DB

--- components/header.jsx
+++ components/header.jsx
@@ -1,12 +1,27 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import { Button } from './ui/button'
-import { ChevronDown, FileText, GraduationCap, LayoutDashboard, PenBox, StarsIcon } from 'lucide-react'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './ui/dropdown-menu'
+import React from "react";
+import { Button } from "./ui/button";
+import {
+  PenBox,
+  LayoutDashboard,
+  FileText,
+  GraduationCap,
+  ChevronDown,
+  StarsIcon,
+} from "lucide-react";
+import Link from "next/link";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Image from "next/image";
+import { checkUser } from "@/lib/checkUser";
 
-const Header = () => {
+export default async function Header() {
+  await checkUser();
+
   return (
     <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
@@ -92,9 +107,5 @@ const Header = () => {
         </div>
       </nav>
     </header>
-
-
-  )
+  );
 }
-
-export default Header
